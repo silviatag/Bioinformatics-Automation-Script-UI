3ac7f121131ef8009fd5d966e2b00598eef0daa1
Silvia Hassan <silviahassantag@yahoo.com>
2026-01-01 03:55:05 +0200

    removing unfinished pages from nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
-import About from './Pages/about';
-import Documentation from './Pages/documentation';
+// import About from './Pages/about';
+// import Documentation from './Pages/documentation';
 import Tools from './Pages/tools';
 
 function App() {
@@ -17,16 +17,16 @@ function App() {
           <div className="navbar-right">
             <Link to="/Home">Home</Link>
             <Link to="/tools">Tools</Link>
-            <Link to="/documentation">Documentation</Link>
-            <Link to="/about">About Us</Link>
+            {/* <Link to="/documentation">Documentation</Link>
+            <Link to="/about">About Us</Link> */}
           </div>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/documentation" element={<Documentation />} />
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* <Route path="/documentation" element={<Documentation />} /> */}
           <Route path="/tools" element={<Tools />} />
         </Routes>
       </div>
